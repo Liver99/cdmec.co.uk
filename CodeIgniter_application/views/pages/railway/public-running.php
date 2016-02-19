7b471be007483e82b39cfcabe20ad58f26b96c56
Michael <michael@michaelkavanagh.me>
2016-02-19 12:06:53 +0000

    Update 2016 running days

--- CodeIgniter_application/views/pages/railway/public-running.php
+++ CodeIgniter_application/views/pages/railway/public-running.php
@@ -9,8 +9,8 @@
         <div class="perex">
             <p>As a condition of the club's lease with the London Borough of Waltham Forest on whose land the railway is constructed, we operate the track for public running every Sunday Afternoon between April and the end of September.</p>
         </div>
-        
-        <h2>Railway public running days 2015</h2>
+
+        <h2>Railway public running days 2016</h2>
         <p>Summer afternoons from 2pm till 5:30pm. The full timetable can be seen below:</p>
         <table width="100%">
             <tr>
@@ -23,95 +23,103 @@
             <tr>
                 <td>&nbsp;</td>
                 <td>&nbsp;</td>
-                <td>5<sup>th</sup> April</td>
-                <td>12<sup>th</sup> April</td>
-                <td>19<sup>th</sup> April</td>
-            </tr>
-            <tr>
-                <td>26<sup>th</sup> April</td>
-                <td>3<sup>rd</sup> May</td>
-                <td>10<sup>th</sup> May</td>
-                <td>17<sup>th</sup> May</td>
-                <td>24<sup>th</sup> May</td>
-            </tr>
-            <tr>
-                <td>31<sup>st</sup> May</td>
-                <td>7<sup>th</sup> June</td>
-                <td>14<sup>th</sup> June</td>
-                <td>21<sup>st</sup> June</td>
-                <td>28<sup>th</sup> June</td>
-            </tr>
-            <tr>
-                <td>5<sup>th</sup> July</td>
-                <td>12<sup>th</sup> July</td>
-                <td>19<sup>th</sup> July</td>
-                <td>26<sup>th</sup> July</td>
-                <td>2<sup>nd</sup> August</td>
-            </tr>
-            <tr>
-                <td>9<sup>th</sup> August</td>
-                <td>16<sup>th</sup> August</td>
-                <td>23<sup>rd</sup> August</td>
-                <td>30<sup>th</sup> August</td>
-                <td>6<sup>th</sup> September</td>
-            </tr>
-            <tr>
-                <td>13<sup>th</sup> September</td>
-                <td>20<sup>th</sup> September</td>
-                <td>27<sup>th</sup> September</td>
                 <td>&nbsp;</td>
+                <td>3<sup>rd</sup> April</td>
+                <td>10<sup>th</sup> April</td>
+            </tr>
+            <tr>
+                <td>17<sup>th</sup> April</td>
+                <td>24<sup>th</sup> April</td>
+                <td>1<sup>st</sup> May</td>
+                <td>8<sup>th</sup> May</td>
+                <td>15<sup>th</sup> May</td>
+            </tr>
+            <tr>
+                <td>22<sup>nd</sup> May</td>
+                <td>29<sup>th</sup> May</td>
+                <td>5<sup>th</sup> June</td>
+                <td>12<sup>th</sup> June</td>
+                <td>19<sup>th</sup> June</td>
+            </tr>
+            <tr>
+                <td>26<sup>th</sup> June</td>
+                <td>3<sup>rd</sup> July</td>
+                <td>10<sup>th</sup> July</td>
+                <td>17<sup>th</sup> July</td>
+                <td>24<sup>th</sup> July</td>
+            </tr>
+            <tr>
+                <td>31<sup>st</sup> July</td>
+                <td>7<sup>th</sup> August</td>
+                <td>14<sup>th</sup> August</td>
+                <td>21<sup>st</sup> August</td>
+                <td>28<sup>th</sup> August</td>
+            </tr>
+            <tr>
+                <td>4<sup>th</sup> September</td>
+                <td>11<sup>th</sup> September</td>
+                <td>18<sup>th</sup> September</td>
+                <td>25<sup>th</sup> September</td>
                 <td>&nbsp;</td>
             </tr>
         </table>
-        <h2>Bank Holiday/Scratch Rota Running Days 2015</h2>
+        <h2>Bank Holiday/Scratch Rota Running Days 2016</h2>
         <p>The club also runs full passenger service on bank holiday Monday's</p>
         <table>
             <tr>
                 <th colspan="2">Scratch Rota<br /><b>Bank Holidays</b></th>
             </tr>
             <tr>
-                <td width="50%">6<sup>th</sup> April</td>
+                <td width="50%">27<sup>th</sup> March</td>
                 <td width="50%">Raised Track</td>
             </tr>
             <tr>
-                <td>4<sup>th</sup> May</td>
+                <td>28<sup>th</sup> March</td>
                 <td>Ground Level Track</td>
             </tr>
             <tr>
-                <td>25<sup>th</sup> May</td>
+                <td>2<sup>nd</sup> May</td>
                 <td>Raised Track</td>
             </tr>
             <tr>
-                <td>31<sup>st</sup> August</td>
+                <td>30<sup>th</sup> May</td>
+                <td>Ground Level Track</td>
+            </tr>
+            <tr>
+                <td>29<sup>th</sup> August</td>
                 <td>Ground Level Track</td>
             </tr>
         </table>
 
-        <h2>Wednesday Summer Running Days 2015</h2>
+        <h2>Wednesday Summer Running Days 2016</h2>
         <p>We will be running a full service on Wednesday afternoons from 2pm till 4:00pm during the summer holidays</p>
         <table>
             <tr>
                 <th colspan="2">Wednesdays<br /><b>Summer Holidays</b></th>
             </tr>
             <tr>
-                <td width="50%">29<sup>th</sup> July</td>
+                <td width="50%">27<sup>th</sup> July</td>
                 <td width="50%">Ground Level Track</td>
             </tr>
             <tr>
-                <td>5<sup>th</sup> August</td>
-                <td>Raised Track</td>
-            </tr>
-            <tr>
-                <td>12<sup>th</sup> August</td>
+                <td>3<sup>rd</sup> August</td>
                 <td>Ground Level Track</td>
             </tr>
             <tr>
-                <td>19<sup>th</sup> August</td>
+                <td>10<sup>th</sup> August</td>
+                <td>Raised Track</td>
+            </tr>
+            <tr>
+                <td>17<sup>th</sup> August</td>
                 <td>Ground Level Track</td>
             </tr>
             <tr>
-                <td>26<sup>th</sup> August</td>
+                <td>24<sup>th</sup> August</td>
                 <td>Raised Track</td>
+            </tr>
+            <tr>
+                <td>31<sup>st</sup> August</td>
+                <td>Ground Level Track</td>
             </tr>
         </table>
 
