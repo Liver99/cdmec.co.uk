982ac1cc33e427f610418b5e0c55b4c9bdcff9fb
Michael <michael@michaelkavanagh.me>
2016-04-17 20:07:40 +0100

    Highlight next running day

--- js/public-running.js
+++ js/public-running.js
@@ -1,8 +1,37 @@
 var UNIXtime = Date.now();// get Unix timestamp for this instant (ms)
+var datesArray=Array();
 
-$('td').each(function(index, value){
-  var date = new Date( $(this).data('date') ).getTime();
-  if (UNIXtime <= date) {
-      $(this).addClass('success');
+$('.date').each(function(index, value){  // for data-date
+  var runningDate = new Date( $(this).data('date') ).getTime();  // get date from data-date
+  if (UNIXtime <= runningDate) {                // if date is in the future
+    datesArray.push(runningDate);
   }
 });
+var closestDate = closest(UNIXtime, datesArray);
+
+$('.date').each(function(index, value){  // for each table cell
+  var runningDate = new Date( $(this).data('date') ).getTime();  // get date from data-date
+  if (UNIXtime <= runningDate) {                // if date is in the future
+    if (runningDate==closestDate){
+      $(this).addClass('success');
+    } else {
+      $(this).addClass('active');
+    }
+  } else {                                     // date in the past
+    $(this).addClass('text-muted');
+  }
+});
+
+
+function closest (num, arr) {
+  var curr = arr[0];
+  var diff = Math.abs (num - curr);
+  for (var val = 0; val < arr.length; val++) {
+    var newdiff = Math.abs (num - arr[val]);
+    if (newdiff < diff) {
+      diff = newdiff;
+      curr = arr[val];
+    }
+  }
+  return curr;
+}
